a81663daa2bc48f0345814f4bf5c9138141e8827
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-12-05 22:26:04 +0530

    all functionality done execpt timer

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,8 +1,8 @@
 function WelcomePage({ noOfQuestions, dispatch }) {
     return (
-        <div style={{ textAlign: 'center' }}>
+        <div style={{ textAlign: 'center',lineHeight:'50px' }}>
             <h1>Welcome to the QUIZ</h1>
-            <p>{noOfQuestions} questions to test your react knowledge</p>
+            <p style={{fontSize:'25px'}}><b>{noOfQuestions}</b><em> questions to test your react knowledge</em></p>
             <button className="startBtn" onClick={() => { dispatch({ type: 'start' }) }}>Start</button>
         </div>
     );
