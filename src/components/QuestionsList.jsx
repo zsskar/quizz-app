38c3399cdd349d577b8d9091079152ee2bfa05fd
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-12-05 22:26:31 +0530

    all functionality done execpt timer

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -4,14 +4,13 @@ export default function QuestionsList({ question, dispatch, noOfQuestions, quest
     return (
         <>
             <div className='questions'>
-                <h2 style={{marginLeft:'6%'}}>Q- <em>{question.question}</em></h2>
+                <h2 style={{ marginLeft: '6%' }}>Q- <em>{question.question}</em></h2>
                 <ul>
                     {
                         question.options.map((option, index) =>
                             <button key={index} onClick={() => { dispatch({ type: 'answer', payload: index }) }}
-                                className={`${answer != null ? 'disable':''} ${answer != null ?answer === question.answer && index === answer ? 'right selected' :
-                                    answer !== question.answer && index === answer ? 'wrong selected' : 'restQuestions' : ''}`}
-                                >
+                                className={`${answer != null ? 'disable' : ''} ${answer != null ? answer === question.answer && index === answer ? 'right selected' :
+                                    answer !== question.answer && index === answer ? 'wrong selected' : 'restQuestions' : ''}`}>
                                 {option}
                             </button>
                         )}
