d930108a53d094c1629d248089193a4937d2131b
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-12-06 19:50:01 +0530

    timer added for each question

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
+
 export default function Footer({ dispatch, noOfQuestions, questionIndex }) {
+
     const check = questionIndex < noOfQuestions - 1;
+    const [timeLeft, setTimeLeft] = useState(10);
+    useEffect(() => {
+        if (!timeLeft) {
+            console.log("time becomes zero !", timeLeft);
+            check ? dispatch({ type: 'next' }) : dispatch({ type: 'finish' });
+            setTimeLeft(10);
+            return;
+        }
+        const intervalId = setInterval(() => {
+            setTimeLeft(timeLeft - 1);
+        }, 1000);
+        return () => clearInterval(intervalId);
+    }, [timeLeft, dispatch, check]);
+
     return (
         <div className="buttons">
-            {<button className="timer">5:00</button>}
-            {check && <button className="next" onClick={() => { dispatch({ type: 'next' }) }}>Next</button>}
+            {<button className="timer"><span className={`blink ${timeLeft <= 5 ? 'timeOver' : 'timeLeft'}`}>{timeLeft} </span>sec</button>}
+            {check && <button className="next" onClick={() => { dispatch({ type: 'next' }); setTimeLeft(10) }}>Next</button>}
             {!check && <button style={{ marginLeft: '70%' }} className="startBtn" onClick={() => dispatch({ type: 'finish' })}>Finish</button>}
         </div>
     );
